871d5d164a06dbf64e1d19b5e7bf6add99acf0d7
Aurélien Chevillotte <chevillotte.aurelien@gmail.com>
2021-06-14 16:08:11 +0200

    refactorisation avec promesses crawler

--- old/backend/server/strategies/crawler.js
+++ old/backend/server/strategies/crawler.js
@@ -13,6 +13,7 @@ return module.exports = {
     app: null,
     socket: null,
     name: 'Crawler Web',
+    current: 0,
 
     /**
      * Init. function
@@ -34,7 +35,7 @@ return module.exports = {
 
         var pagesitemap = config['url'];
 
-            this.navigateSiteMap(pagesitemap, callback);
+        this.navigateSiteMap(pagesitemap, callback);
 
 
         return '@todo'
@@ -45,43 +46,93 @@ return module.exports = {
      * @param pagesitemap
      * @param callback
      *
-     * Make the difference with the Urlset and sitemapindex
-     */
-    navigateSiteMap: function (pagesitemap, callback) {
-        var _this = this;
-        console.log('On ouvre la page : ' + pagesitemap);
+    //  * Make the difference with the Urlset and sitemapindex
+    //  */
+    // navigateSiteMap: function (pagesitemap, callback) {
+    //     var _this = this;
+    //     console.log('On ouvre la page : ' + pagesitemap);
 
-        // @todo : lire le fichier XML "pagesitemap" et récupérer les URLs qu'il contient
-        console.log(pagesitemap);
+    //     // @todo : lire le fichier XML "pagesitemap" et récupérer les URLs qu'il contient
+    //     // console.log(pagesitemap);
+    //     request.get(
+    //         pagesitemap,
+    //         function (error, response, body) {
+    //             if (typeof body == 'undefined' || error) {
+    //                 // console.log('error while opening sitemap');
+    //                 // console.log(error);
+    //                 process.exit(1);
+    //             }
+
+    //             // console.log('Page ouverte.');
+    //             // console.log(body);
+
+    //             xml2js.parseString(body, async function (err, result) {
+    //                 if (typeof result == 'undefined' || err) {
+    //                     // console.log(err);
+    //                     process.exit(1);
+    //                 }
+
+    //                 if (typeof result.urlset == 'undefined') {
+    //                         for (element of result.sitemapindex.sitemap) {
+
+    //                             console.log('element loc', element.loc);
+    //                             _this.navigateSiteMap(element.loc[0], callback)
+    //                         }
+    //                         // result.sitemapindex.sitemap.forEach(element => {
+
+    //                         // });
+    //                 } else {
+    //                     _this.navigateUrls(result.urlset.url, callback)
+    //                 }
+    //             });
+
+    //         }
+    //     );
+    // },
+
+    navigateSiteMap: async function (pagesitemap) {
+        var _this = this;
+        // console.log('On ouvre la page : ' + pagesitemap);
         request.get(
             pagesitemap,
             function (error, response, body) {
                 if (typeof body == 'undefined' || error) {
-                    console.log('error while opening sitemap');
-                    // console.log(error);
                     process.exit(1);
                 }
 
-                // console.log('Page ouverte.');
-                // console.log(body);
-
                 xml2js.parseString(body, async function (err, result) {
                     if (typeof result == 'undefined' || err) {
-                        console.log(err);
+
                         process.exit(1);
                     }
 
                     if (typeof result.urlset == 'undefined') {
-                            for (element of result.sitemapindex.sitemap) {
+                        let resultUrl = [];
+                        for (let element of result.sitemapindex.sitemap) {
+                            // console.log('element loc', element.loc);
+                            // console.log('element', element);
+                            const result = await element.loc
+                            console.log('result', result)
+                            await resultUrl.push(result)
 
-                                console.log('sitemap', element.loc);
-                                _this.navigateSiteMap(element.loc[0], callback)
-                            }
-                            // result.sitemapindex.sitemap.forEach(element => {
+                        }
+                        console.log('tableauurlresult', resultUrl)
+                        // let el1 = await _this.navigateSiteMap(resultUrl)
+                        for (let url of resultUrl) {
+                            console.log('tabel', url)
+                            let el1 = _this.navigateSiteMap(url[0])
+                            // let el2 = await el1
 
-                            // });
+                            // const element1 = await el1
+                            console.log('el1', el1)
+                            // console.log('el2', el2)
+                        }
+                        // result.sitemapindex.sitemap.forEach(element => {
+
+                        // });
                     } else {
-                        _this.navigateUrls(result.urlset.url, callback)
+                        _this.current = 0;
+                        _this.navigateUrls(result.urlset.url)
                     }
                 });
 
@@ -89,14 +140,17 @@ return module.exports = {
         );
     },
 
+
     /**
      * Allow the crawler to navigate
      */
     navigateUrls: function (urlList, callback) {
 
+
         var url = urlList.shift();
         var _this = this;
         var now = new Date();
+        _this.current++;
 
         url = url.loc[0];
 
@@ -106,12 +160,12 @@ return module.exports = {
                 var data = url + "," + response.statusCode + "," + " " + date.format(now, 'YYYY/MM/DD HH:mm:ss') + '\n';
                 // console.log(error);
                 // console.log(response);
-                console.log('data', data);
+                // console.log('data', data);
                 _this.socket.emit('urlFromBack', data)
                 // console.log('emission ok');
 
                 fs.appendFileSync('var/log/urls.txt', data);
-                if (urlList.length > 0) {
+                if (urlList.length > 0 && _this.current < 5) {
                     _this.navigateUrls(urlList, callback);
                 } else if (callback) {
                     callback();
