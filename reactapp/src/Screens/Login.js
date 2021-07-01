2c7961ded1b1fea8235c8077c1ed943b32d6b780
Aurélien Chevillotte <chevillotte.aurelien@gmail.com>
2021-07-01 10:41:56 +0200

    route pour login et redirection home okay

--- reactapp/src/Screens/Login.js
+++ reactapp/src/Screens/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { Redirect } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
@@ -18,6 +19,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Login() {
+
+    //variable d'état pour les inputs et infos à envoyer au back
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
@@ -25,34 +28,95 @@ function Login() {
     const [newEmail, setNewEmail] = useState("");
     const [newPassword, setNewPassword] = useState("");
 
-    // const validate = (email) => {
-    //     const expression =
-    //         /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-    //     return expression.test(String(email).toLowerCase());
-    // };
+    //variable d'état pour vérifier si un utilisateur existe ou pas
+    const [userExist, setUserExist] = useState(false);
 
-    const handleClickSignIn = async () => {
-        console.log('click sign in okay')
-        const data = await fetch('http://localhost:3000/account/signin', {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body : `emailFromFront=${email}&passwordFromFront=${password}`
-        })
-    } 
-
-    const handleClickSignUp = async () => {
-        console.log('click signup okay')
-        const data = await fetch('http://localhost:3000/account/signup', {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body : `emailFromFront=${newEmail}&passwordFromFront=${newPassword}&nameFromFront=${name}&firstNameFromFront=${firstName}`
-        })
-
-        const body = await data.json()
-        console.log("body", body)
-    }
+    //variable d'états pour les erreurs
+    const [errorsListSignUp, setErrorsListSignUp] = useState([]);
+    const [errorsListSignIn, setErrorsListSignIn] = useState([]);
 
     const classes = useStyles();
+
+    //Regex pour vérifier que l'email est correct
+    const validate = (email) => {
+        const expression =
+            /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        return expression.test(String(email).toLowerCase());
+    };
+
+    //traitement de l'information pour un utilisateur qui a deja un compte
+    const handleClickSignIn = async () => {
+        console.log('click sign in okay')
+
+        if (validate(email) === false) {
+            setErrorsListSignIn(["Le format de l'adresse mail n'est pas valide"])
+        } else {
+            const data = await fetch('/account/signin', {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: `emailFromFront=${email}&passwordFromFront=${password}`
+            })
+
+            const body = await data.json()
+            console.log("body", body)
+
+            if (body.result === true) {
+                setUserExist(true)
+            } else {
+                setErrorsListSignIn(body.error)
+            }
+        }
+    }
+
+    //traitement de l'information pour un utilisateur qui créer un compte
+    const handleClickSignUp = async () => {
+        console.log('click signup okay')
+
+        if (validate(newEmail) === false) {
+            setErrorsListSignUp(["Le format de l'adresse mail n'est pas valide"])
+        } else {
+            const data = await fetch('/account/signup', {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: `emailFromFront=${newEmail}&passwordFromFront=${newPassword}&nameFromFront=${name}&firstNameFromFront=${firstName}`
+            })
+
+            const body = await data.json()
+            console.log("body", body)
+
+            if (body.result === true) {
+                setUserExist(true)
+            } else {
+                setErrorsListSignUp(body.error)
+            }
+        }
+    }
+
+    //redirection sur la page home si le backend renvoit un result = true
+    if (userExist) {
+        return (
+            <Redirect to='/home' />
+        )
+    }
+
+    //affichage des erreurs pour signup
+    let errorSignUp = errorsListSignUp.map((errorItem, i) => {
+        return (
+            <Row>
+                <p key={i} className="error">{errorItem}</p>
+            </Row>
+        )
+    })
+
+    //affichage des erreurs pour sign in
+    let errorSignIn = errorsListSignIn.map((errorItem, i) => {
+        return (
+            <Row>
+                <p key={i} className="error">{errorItem}</p>
+            </Row>
+        )
+    })
+
 
     return (
         <Container fluid>
@@ -99,7 +163,8 @@ function Login() {
                             value={password}
                         />
                     </Row>
-                    <Button color="primary" size="lg" className="button" block onClick={() => handleClickSignIn()}>
+                    {errorSignIn}
+                    <Button color="primary" size="lg" className="button" onClick={() => handleClickSignIn()}>
                         Je me connecte
                     </Button>
                 </Col>
@@ -170,7 +235,8 @@ function Login() {
                         />
                     </Row>
                     <Row>
-                        <Button color="primary" size="lg" className="button" block onClick={() => handleClickSignUp()}>
+                        {errorSignUp}
+                        <Button color="primary" size="lg" className="button" onClick={() => handleClickSignUp()}>
                             Je créer mon compte
                         </Button>
                     </Row>
--- routes/account.js
+++ routes/account.js
@@ -16,38 +16,42 @@ router.post('/signup', async (req, res, next) => {
     // SELECT * FROM `login`
     let error = []
     let result = false
-    let saveUser = null
+    // let saveUser = null
     let token = null
 
+    //on recherche par l'adresse mail si l'utilisateur existe en BDD
     const data = await UserModel.findOne({
         where: { email: req.body.emailFromFront }
     })
 
     console.log('data', data)
 
+    //si l'utilisateur existe en bdd on renvoit une erreur
     if (data != null) {
         error.push('utilisateur déjà présent')
     }
 
+    //si l'un des champs est vide on renvoit une erreur
     if (req.body.emailFromFront === '' || req.body.password === '' || req.body.nameFromFront === '' || req.body.firstNameFromFront === '') {
-        error.push('champs vides')
+        error.push('Merci de remplir tous les champs!')
     }
 
-    if (error.length===0) {
+    //si il n'y a aucunes erreurs on créer un nouvel utilisateur 
+    if (error.length === 0) {
         const hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
 
         let newAccount = UserModel.build({
             nom: req.body.nameFromFront,
             prenom: req.body.firstNameFromFront,
             email: req.body.emailFromFront,
-            password : hash,
-            token : uid2(32)
+            password: hash,
+            token: uid2(32)
         })
 
         saveNewAccount = await newAccount.save()
         console.log('saveAccount', saveNewAccount)
 
-        if(saveNewAccount) {
+        if (saveNewAccount) {
             result = true
             token = saveNewAccount.dataValues.token
         }
@@ -55,11 +59,43 @@ router.post('/signup', async (req, res, next) => {
         console.log('token', token)
     }
 
-    res.json({token, result, error, saveNewAccount})
+    res.json({ token, result, error })
 });
 
 router.post('/signin', async (req, res, next) => {
-    // console.log(req.body)
-})
+    console.log(req.body)
+
+    let error = []
+    let result = false
+    let token = null
+    let user = null
+
+    //on renvoit une erreur si l'un des champs est vide
+    if (req.body.emailFromFront === '' || req.body.passwordFromFront === '') {
+        error.push('Merci de remplir tous les champs!')
+    }
+
+    //si il n'y a pas d'erreur on cherche l'utilisateur en BDD avec son email
+    if (error.length === 0) {
+        user = await UserModel.findOne({
+            where: { email: req.body.emailFromFront }
+        })
+    }
+
+    //si on trouve un utilisateur en bdd on compare son mdp si celui-ci est faux on renvoit une erreur
+    if (user) {
+        if (bcrypt.compareSync(req.body.passwordFromFront, user.password)) {
+            result = true
+            token = user.token
+        } else {
+            result = false
+            error.push('mot de passe incorrect')
+        }
+    } else {
+        error.push('email incorrect')
+    }
+
+    res.json({token, result, error})
+});
 
 module.exports = router;
